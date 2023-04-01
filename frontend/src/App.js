d28d64e2eb1da4310e73b00322649f536ffee09c
Aryan <aryan.khubchandani@gmail.com>
2023-04-01 07:50:05 +0530

    chore: update routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,23 +9,28 @@ import Form from "./Components/Form/Form";
 import Account from "./Components/Account/Account";
 import Package from "./Components/Package/Package";
 
+import { Provider } from "react-redux";
+import store from "./Components/Redux/store";
+
 function App() {
   return (
-    <div className="App">
-      <Router>
-        <Navbar />
-        <div className="mb-24" />
-        <Routes>
-          <Route path="/" element={<Landing />} exact />
-          <Route path="/signup" element={<Signup />} exact />
-          <Route path="/login" element={<Login />} exact />
-          <Route path="/form" element={<Form />} exact />
-          <Route path="/account" element={<Account />} exact />
-          <Route path="/package" element={<Package />} exact />
-          <Route path="/profile" element={<Profile />} exact />
-        </Routes>
-      </Router>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <Router>
+          <Navbar />
+          <div className="mb-24" />
+          <Routes>
+            <Route path="/" element={<Login />} exact />
+            <Route path="/signup" element={<Signup />} exact />
+            {/* <Route path="/login" element={<Login />} exact /> */}
+            <Route path="/form" element={<Form />} exact />
+            <Route path="/account" element={<Account />} exact />
+            <Route path="/package" element={<Package />} exact />
+            {/* <Route path="/profile" element={<Profile />} exact /> */}
+          </Routes>
+        </Router>
+      </div>
+    </Provider>
   );
 }
 
