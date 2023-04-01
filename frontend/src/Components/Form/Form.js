f4a255f07ab07ba367fada9532f28c3ed32afd4d
Aryan <aryan.khubchandani@gmail.com>
2023-04-01 07:44:22 +0530

    feat: add form

--- frontend/src/Components/Form/Form.js
+++ frontend/src/Components/Form/Form.js
@@ -1,17 +1,41 @@
 import react from "react";
+import { Link } from "react-router-dom";
+
+import "./Form.css";
 
 const Form = () => {
   return (
     <div className="form-container">
-      <div className="form-title">Signup Form</div>
-      {/* Sign up form for farm insurance */}
+      <div className="form-title">
+        <h1>
+          <b>SIGNUP FORM</b>{" "}
+        </h1>
+      </div>
+      <br />
+      <div className="font-bold">
+        Sign up for Agrisure-farm insurance from here:
+      </div>
+
       <div className="form-content">
         <div className="form-content-left">
-          <div className="form-content-left-title">Farm Details</div>
+          <h2 align="center">
+            <b>FARMER PERSONAL DETAILS</b>
+          </h2>
+
+          <div className="form-content-left-title">Aadhar Number</div>
           <div className="form-content-left-content">
             <div className="form-content-left-content-farm">
+              <div className="form-content-left-content-farm-input">
+                <input
+                  type="text"
+                  className="form-content-left-content-farm-input-field"
+                />
+              </div>
+            </div>
+            <br />
+            <div className="form-content-left-content-farm">
               <div className="form-content-left-content-farm-title">
-                Farm Name
+                Farmer Name
               </div>
               <div className="form-content-left-content-farm-input">
                 <input
@@ -20,9 +44,10 @@ const Form = () => {
                 />
               </div>
             </div>
+            <br />
             <div className="form-content-left-content-farm">
               <div className="form-content-left-content-farm-title">
-                Farm Address
+                District
               </div>
               <div className="form-content-left-content-farm-input">
                 <input
@@ -31,50 +56,90 @@ const Form = () => {
                 />
               </div>
             </div>
+            <br />
             <div className="form-content-left-content-farm">
               <div className="form-content-left-content-farm-title">
-                Farm Area
-              </div>
-              <div className="form-content-left-content-farm-input">
-                <input
-                  type="text"
-                  className="form-content-left-content-farm-input-field"
-                />
-              </div>
-            </div>
-            <div className="form-content-left-content-farm">
-              <div className="form-content-left-content-farm-title">
-                Farm Type
+                Farmer Category
               </div>
               <div className="form-content-left-content-farm-input">
                 <select className="form-content-left-content-farm-input-field">
                   <option value="1">Select</option>
-                  <option value="2">Option 2</option>
-                  <option value="3">Option 3</option>
-                  <option value="4">Option 4</option>
+                  <option value="2">Small</option>
+                  <option value="3">Marginal</option>
+                  <option value="4">Big</option>
                 </select>
               </div>
             </div>
+            <br />
             <div className="form-content-left-content-farm">
               <div className="form-content-left-content-farm-title">
-                Farm Crop
+                Bank Account Number
               </div>
               <div className="form-content-left-content-farm-input">
-                <select className="form-content-left-content-farm-input-field">
-                  <option value="1">Select</option>
-                  <option value="2">Option 2</option>
-                  <option value="3">Option 3</option>
-                </select>
+                <input
+                  type="text"
+                  className="form-content-left-content-farm-input-field"
+                />
+              </div>
+              <br />
+              <div className="form-content-left-content-farm">
+                <div className="form-content-left-content-farm-title">
+                  Village/Ward of Farmer
+                </div>
+                <div className="form-content-left-content-farm-input">
+                  <input
+                    type="text"
+                    className="form-content-left-content-farm-input-field"
+                  />
+                </div>
+                <br />
+                <div className="form-content-left-content-farm">
+                  <div className="form-content-left-content-farm-title">
+                    Mobile Number
+                  </div>
+                  <div className="form-content-left-content-farm-input">
+                    <input
+                      type="text"
+                      className="form-content-left-content-farm-input-field"
+                    />
+                  </div>
+                  <br />
+                  <div className="form-content-left-content-farm">
+                    <div className="form-content-left-content-farm-title">
+                      Community Category
+                    </div>
+                    <br />
+                    <div className="form-content-left-content-farm-input">
+                      <select className="form-content-left-content-farm-input-field">
+                        <option value="1">Select</option>
+                        <option value="2">SC/ST</option>
+                        <option value="3">OBC</option>
+                        <option value="4">General</option>
+                      </select>
+                    </div>
+                    <br />
+                  </div>
+                </div>
               </div>
             </div>
           </div>
         </div>
         <div className="form-content-right">
-          <div className="form-content-right-title">Farm Owner Details</div>
+          <h2 className="font-bold">
+            LAND HOLDING PARTICULARS(AS PER PASSBOOK)
+          </h2>
+          <div className="form-content-right-title">District</div>
+          <input
+            type="text"
+            className="form-content-right-content-farm-input-field"
+          />
+          <br />
 
           <div className="form-content-right-content">
             <div className="form-content-right-content-farm">
-              <div className="form-content-right-content-farm-title">Name</div>
+              <div className="form-content-right-content-farm-title">
+                Mandal
+              </div>
               <div className="form-content-right-content-farm-input">
                 <input
                   type="text"
@@ -82,27 +147,10 @@ const Form = () => {
                 />
               </div>
             </div>
-            <div className="form-content-right-content-farm">
-              <div className="form-content-right-content-farm-title">Email</div>
-              <div className="form-content-right-content-farm-input">
-                <input
-                  type="text"
-                  className="form-content-right-content-farm-input-field"
-                />
-              </div>
-            </div>
-            <div className="form-content-right-content-farm">
-              <div className="form-content-right-content-farm-title">Phone</div>
-              <div className="form-content-right-content-farm-input">
-                <input
-                  type="text"
-                  className="form-content-right-content-farm-input-field"
-                />
-              </div>
-            </div>
+            <br />
             <div className="form-content-right-content-farm">
               <div className="form-content-right-content-farm-title">
-                Address
+                Survey Number
               </div>
               <div className="form-content-right-content-farm-input">
                 <input
@@ -111,34 +159,98 @@ const Form = () => {
                 />
               </div>
             </div>
+            <br />
             <div className="form-content-right-content-farm">
-              <div className="form-content-right-content-farm-title">State</div>
+              <div className="form-content-right-content-farm-title">
+                Extent
+              </div>
               <div className="form-content-right-content-farm-input">
-                <select className="form-content-right-content-farm-input-field">
-                  <option value="1">Select</option>
-                  <option value="2">Option 2</option>
-                  <option value="3">Option 3</option>
-                  <option value="4">Option 4</option>
+                <input
+                  type="text"
+                  className="form-content-right-content-farm-input-field"
+                />
+                <select className="form-content-left-content-farm-input-field">
+                  <option value="1">Acres</option>
+                  <option value="2">Cents</option>
+                  <option value="3">Guntas</option>
                 </select>
               </div>
             </div>
-            <div className="form-content-right-content-farm">
-              <div className="form-content-right-content-farm-title">City</div>
-              <div className="form-content-right-content-farm-input">
-                <select className="form-content-right-content-farm-input-field">
-                  <option value="1">Select</option>
-                  <option value="2">Option 2</option>
-                  <option value="3">Option 3</option>
-                  <option value="4">Option 4</option>
-                </select>
+            <br />
+            <div className="form-content-left-content-farm">
+              <div className="form-content-left-content-farm-title">
+                Name of Existing Insurance on Farm,if any:
               </div>
+              <div className="form-content-left-content-farm-input">
+                <input
+                  type="text"
+                  className="form-content-left-content-farm-input-field"
+                />
+              </div>
+            </div>
+            <br />
+            <h2 className="font-bold" align="center">
+              CROP DETAILS
+            </h2>
+            <div className="form-content-left-title">Name of the Crop:</div>
+            <div className="form-content-left-content">
+              <div className="form-content-left-content-farm">
+                <div className="form-content-left-content-farm-title"></div>
+                <div className="form-content-left-content-farm-input">
+                  <input
+                    type="text"
+                    className="form-content-left-content-farm-input-field"
+                  />
+                </div>
+              </div>
+              <br />
+              <div className="form-content-left-content-farm">
+                <div className="form-content-left-content-farm-title">
+                  Area grown(in acres)
+                </div>
+                <div className="form-content-left-content-farm-input">
+                  <input
+                    type="text"
+                    className="form-content-left-content-farm-input-field"
+                  />
+                </div>
+              </div>
+              <br />
+              <div className="form-content-left-content-farm">
+                <div className="form-content-left-content-farm-title">
+                  Time Period of Sowing
+                </div>
+                <div className="form-content-left-content-farm-input">
+                  <input
+                    type="text"
+                    className="form-content-left-content-farm-input-field"
+                  />
+                </div>
+              </div>
+              <br />
+              <div className="form-content-left-content-farm">
+                <div className="form-content-left-content-farm-title">
+                  Type of Crop
+                </div>
+                <div className="form-content-left-content-farm-input">
+                  <select className="form-content-left-content-farm-input-field">
+                    <option value="1">Select</option>
+                    <option value="2">Rabi</option>
+                    <option value="3">Kharif</option>
+                  </select>
+                </div>
+              </div>
+              <br />
+              <br />
             </div>
           </div>
         </div>
       </div>
       <div className="form-footer">
         <div className="form-footer-button">
-          <button className="form-footer-button-submit">Submit</button>
+          <Link to="/account">
+            <button className="form-footer-button-submit">Submit</button>
+          </Link>
         </div>
       </div>
     </div>
