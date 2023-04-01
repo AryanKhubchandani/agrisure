34fb904a60dae4f19b2a4a299788f079bbc01190
Aryan <aryan.khubchandani@gmail.com>
2023-04-01 07:46:18 +0530

    feat: update account page

--- frontend/src/Components/Account/Account.js
+++ frontend/src/Components/Account/Account.js
@@ -1,18 +1,31 @@
 import react from "react";
 import Razorpay from "./razorpay";
+
 import "./Account.css";
 
-const Account = () => {
+import { connect } from "react-redux";
+import { useState } from "react";
+
+import user from "../../assets/images/user.png";
+
+const Account = ({ account, contract }) => {
+  const [check, setCheck] = useState(false);
   // get coins in account
-  const getCoins = () => {
-    return 14;
-    // fetch coins from backend
-    // set coins in state
+  const [data, setData] = useState();
+  let d = 0;
+  const getCoins = async () => {
+    setData(await contract.methods.balanceOf(account).call());
+    console.log(data);
+    d = data;
+    return data;
   };
 
-  const razorpay = () => {
-    razorpay
-  }
+  const buyAGS = async () => {
+    const amount = 10000; //input from frontend
+    const data = await contract.methods.buyAGS(amount).send({ from: account });
+    console.log(data);
+    setCheck(true);
+  };
 
   return (
     <div className="account-container">
@@ -20,16 +33,26 @@ const Account = () => {
       <div className="account-section">
         <div className="username-section">
           <div className="account-image">
-            <img src="https://i.imgur.com/6kzYQ2x.png"></img>
+            <img className="w-14 h-14" src={user}></img>
           </div>
-          <div className="account-username">Username</div>
+          <div className="account-username">Aryan</div>
         </div>
         <div className="account-balance">
           <div className="account-balance-title">Total Balance</div>
-          <div className="account-balance-amount">{getCoins()} AGC</div>
+          <div className="account-balance-amount">{data} AGC</div>
+          <button className="get-balance" onClick={getCoins}>
+            {" "}
+            Get Balance
+          </button>
         </div>
         <div className="buy-withdraw">
-          <button className="add-btn" onClick={razorpay}>Buy AGC</button>
+          <div className="flex-row">
+            <button className="add-btn" onClick={buyAGS}>
+              Buy AGC
+            </button>
+            <input type="text"></input>
+          </div>
+          {/* <Razorpay /> */}
           <button className="withdraw-btn">Withdraw</button>
         </div>
       </div>
@@ -42,17 +65,29 @@ const Account = () => {
             <div className="txn-history-table-header-date">Date</div>
             <div className="txn-history-table-header-status">Status</div>
           </div>
-          <div className="txn-history-table-body">
-            <div className="txn-history-table-body-row">
-              <div className="txn-history-table-body-row-amount">0</div>
-              <div className="txn-history-table-body-row-date">0</div>
-              <div className="txn-history-table-body-row-status">0</div>
+          {check == true ? (
+            <div className="txn-history-table-body">
+              <div className="txn-history-table-body-row">
+                <div className="txn-history-table-body-row-amount">10000</div>
+                <div className="txn-history-table-body-row-date">
+                  &nbsp;&nbsp;&nbsp;&nbsp;1/4/2022
+                </div>
+                <div className="txn-history-table-body-row-status">
+                  Buy&nbsp;&nbsp;&nbsp;
+                </div>
+              </div>
             </div>
-          </div>
+          ) : null}
         </div>
       </div>
     </div>
   );
 };
+function mapStateToProps(state) {
+  return {
+    account: state.account,
+    contract: state.contract,
+  };
+}
 
-export default Account;
+export default connect(mapStateToProps)(Account);
